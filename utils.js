1eec4c27dc8ba9dfa7070ec86d3ece29e92dea5f
Rui Alves <up201606746@fe.up.pt>
2020-09-24 21:53:41 +0100

    Add array shuffler

--- utils.js
+++ utils.js
@@ -8,6 +8,25 @@ const generateRandomInt = (from, to) => (
     from + Math.floor(Math.random() * (to - from))
 );
 
+/**
+ * Shuffle an array
+ * @param {Array} arr
+ * @returns {Array} Shuffled array
+ */
+const shuffleArray = (arr) => {
+    const shuffled_arr = [...arr];
+
+    for (let i = shuffled_arr.length - 1; i > 0; --i) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = shuffled_arr[i];
+        shuffled_arr[i] = shuffled_arr[j];
+        shuffled_arr[j] = temp;
+    }
+
+    return shuffled_arr;
+};
+
 module.exports = {
     generateRandomInt,
+    shuffleArray,
 };
