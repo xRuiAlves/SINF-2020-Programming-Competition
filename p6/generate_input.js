66bb9e1c7949af7be3f51fdb9bfa120f70e3efc1
Rui Alves <up201606746@fe.up.pt>
2020-09-28 21:24:42 +0100

    Add Problem 6 input generator

---
+++ p6/generate_input.js
@@ -0,0 +1,16 @@
+const { generateRandomInt, shuffleArray } = require("../utils");
+
+const N = generateRandomInt(2, 31);
+console.info(N);
+
+const seats = [];
+for (let i = 1; i <= N; ++i) {
+    seats.push(i);
+}
+
+for (let i = 0; i < N; ++i) {
+    const num_seats = generateRandomInt(1, N + 1);
+
+    const member_seats = shuffleArray(seats).slice(0, num_seats);
+    console.info(member_seats.join(" "));
+}
